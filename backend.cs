ccff3624ba33ca3a4bca5948457cec42d46a40dd
Hampus Grünewald <hampus.grunewald05@gmail.com>
2023-04-04 11:47:28 +0200

    added live server, and test samples for backend

--- backend.cs
+++ backend.cs
@@ -19,22 +19,23 @@ namespace FreelancerProject
 
     public class person 
     {
-        public string firstName { get; set; }
-        public string surName { get; set; }
+        public string? firstName { get; set; }
+        public string? surName { get; set; }
         public int age { get; set; }
-        public string email { get; set; }
-        public string phoneNumber { get; set; }
-        public string country { get; set; }
-        public string language { get; set; }
-        public string nationality { get; set; }
-        public string driversLicense { get; set; }
-        public string selfDescription{ get; set; }
-        public List<string> abilities { get; set; }
-        public string education { get; set; }
+        public string? email { get; set; }
+        public string? phoneNumber { get; set; }
+        public string? country { get; set; }
+        public string? language { get; set; }
+        public string? nationality { get; set; }
+        public string? driversLicense { get; set; }
+        public string? selfDescription{ get; set; }
+        public List<string>? abilities { get; set; }
+        public string? education { get; set; }
     }
 
     public class CV
     {
+        /*
         public string firstName = Request.Form["firstName"];
         public string surName = Request.Form["surName"];
         public int age = int.Parse(Request.Form["age"]);
@@ -47,13 +48,26 @@ namespace FreelancerProject
         public string selfDescription = Request.Form["selfDescription"];
         public string abilities = Request.Form["abilities"];
         public string education = Request.Form["education"];
+        */
+        public string firstName = "Hampus";
+        public string surName = "Grunewald";
+        public int age = 18;
+        public string email = "hampus.grunewald05@gmail.com";
+        public string phoneNumber = "070 745 41 57";
+        public string country = "Sweden";
+        public string language = "Swedish";
+        public string nationality = "Sweden";
+        public string driversLicense = "yes";
+        public string selfDescription = "";
+        public string abilities = "i can write code";
+        public string education = "i dont have any education";
     }
 
     static class Program
     {   
         public static void Main(string[] args)
-        {
-
+        {   
+            
         }
         
 
